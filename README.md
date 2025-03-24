4dc510e70f82bfa0ec1b75d5084ec2f2e03277fa
PolarGoose <35307286+PolarGoose@users.noreply.github.com>
2025-03-24 16:38:09 +0100

    Use FileMimeTypeDetector from DOpus-Scripting-Extensions to determine MIME-type and Encoding. It greatly improves performance.

--- README.md
+++ README.md
@@ -1,33 +1,28 @@
 # DirectoryOpus-FileMimeTypeAndEncodingColumns-plugin
-This plugin adds two columns, "MIME type" and "Encoding" using [UNIX file](https://man7.org/linux/man-pages/man1/file.1.html) utility.<br>
+This plugin adds two columns, `MIME type` and `Encoding` using `FileMimeTypeDetector` from [DOpus-Scripting-Extensions](https://github.com/PolarGoose/DOpus-Scripting-Extensions).<br>
 The plugin uses the content of a file and not its extension.<br>
 The screenshot below shows how the encoding and MIME type are correctly determined, even for files with incorrect extensions.
 ![Example](doc/screenshot.png)
 
 # Extra Features
-The plugin adds extra information to the MIME or Encoding columns:
-* Architecture of the dll or exe files
-* Dll or exe file is managed (.Net)
-* UTF-8 file has BOM
+* Mime-type aliases can be specified by modifying `mimeTypeAliases` dictionary at the beginning of the script
+
+# Note
+* The encoding doesn't show if a file has BOM or not. You can use [DirectoryOpus-TextFileEncoding-plugin](https://github.com/PolarGoose/DirectoryOpus-TextFileEncoding-plugin) if you need this functionality.
 
 # Prerequisites
-You need to have `file.exe` utility installed on your system. The easiest way to get it is to install [Git for Windows](https://git-scm.com/downloads/win)
+* You need to have DOpus-Scripting-Extensions installed: download it from the [release page](https://github.com/PolarGoose/DOpus-Scripting-Extensions/releases)
 
 # Limitations
-* The script has to call `file.exe` for each file in the folder. Thus, it might be very slow for big folders.
-* UNC paths (paths that start with `\\`) and FTP paths are not supported - the MIME type column will be empty
+* The script doesn't support DOpus portable mode because it requires a PC to have `DOpus-Scripting-Extensions` installed.
+* The plugin is relatively fast but not instantaneous. Thus, for a folder with many files, you might need to wait a little bit for all files to get its MIME type.
 
 # How to use
-* Make sure `file.exe` is installed on your system
+* Make sure [DOpus-Scripting-Extensions](https://github.com/PolarGoose/DOpus-Scripting-Extensions) are installed
 * Download the `js` file from the [latest release](https://github.com/PolarGoose/DirectoryOpus-FileMimeTypeColumn-plugin/releases)
 * Copy the `js` file to the `%AppData%\GPSoftware\Directory Opus\Script AddIns` folder
 * The extra columns will become available in the `Settings`->`File Display Columns`->`Appearance`->`Columns:`.
-* By default, the script uses the `%ProgramFiles%/Git/usr/bin/file.exe` path. If it is installed in a different folder, you need to change the script configuration in the `Script management` menu.
 * The script uses aliases for common MIME types. You can change them by modifying the `mimeTypeAliases` variable at the beginning of the script.
 
-# Portable DOpus use case
-If you use a portable version of DOpus and you don't want to keep 'Git for Windows' installed on your system.<br>
-In that case, you can create a portable `file.exe` distribution:
-* Install `Git for Windows` on your system
-* Copy `%ProgramFiles%/Git/usr/bin/file.exe`, `%ProgramFiles%/Git/usr/bin/*.dll` and `%ProgramFiles%/Git/usr/share/misc/magic.mgc` to the same folder
-* Specify the path to `file.exe` and `magic.mgc` in the script configuration
+# References
+* Discussion of this plugin on DOpus forum: [File MIME type column](https://resource.dopus.com/t/file-mime-type-column/53983)
--- src/file-mime-type-and-encoding-columns.js
+++ src/file-mime-type-and-encoding-columns.js
@@ -1,5 +1,5 @@
 var mimeTypeAliases = {
-  "application/vnd.microsoft.portable-executable": "exe",
+  "application/vnd.microsoft.portable-executable": "exe/dll",
   "application/x-wine-extension-ini": "ini",
   "application/octet-stream": "binary",
   "inode/x-empty": "empty",
@@ -9,26 +9,19 @@ var mimeTypeAliases = {
   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "xlsx",
 }
 
-var shell = new ActiveXObject("WScript.shell")
-var fso = new ActiveXObject("Scripting.FileSystemObject")
-var fsu = DOpus.FSUtil()
-var stt = DOpus.Create().StringTools()
+var fileMimeTypeDetector = new ActiveXObject("DOpusScriptingExtensions.FileMimeTypeDetector")
 
 function OnInit(/* ScriptInitData */ data) {
   data.name = "File_MIME_type_and_Encoding_columns_using_file.exe"
-  data.desc = "Shows the MIME type using the file.exe utility"
+  data.desc = "Shows the MIME type and Encoding using FileMimeTypeDetector from https://github.com/PolarGoose/DOpus-Scripting-Extensions"
   data.default_enable = true
   data.config_desc = DOpus.NewMap()
   data.config_desc("debug") = "Print debug messages to the script log"
   data.config.debug = false
-  data.config_desc("fileExeFullName") = "Path to the 'file.exe' utility. You can get this utility by installing 'Git for Windows': https://git-scm.com/download/win"
-  data.config.fileExeFullName = "%ProgramFiles%/Git/usr/bin/file.exe"
-  data.config_desc("fileExeMagicFileFullName") = "Specify the path to the 'magic.mgc' file. This is only needed if you use a portable version of 'file.exe'. Keep this empty if you use 'Git for Windows'."
-  data.config.fileExeMagicFileFullName = ""
   data.config_desc("ignoreBinaryEncoding") = "Don't show 'binary' encoding. Leave the Encoding column empty instead."
   data.config.ignoreBinaryEncoding = false
   data.version = "0.0-dev";
-  data.url = "https://github.com/PolarGoose/DirectoryOpus-TabLabelizer-plugin";
+  data.url = "https://github.com/PolarGoose/DirectoryOpus-FileMimeTypeAndEncodingColumns-plugin";
 
   var col = data.AddColumn()
   col.name = "MIME type"
@@ -60,48 +53,14 @@ function OnColumnDataRequested(/* ScriptColumnData */ data) {
   var encoding = ""
 
   try {
-    var output = getFileMimeTypeAndEncoding(filePath)
-    debug("output from file.exe: " + output)
-
-    // If file.exe fails it might print "cannot open ..." to the output.
-    if (output.indexOf("cannot open") === 0) {
-      throw "File.exe failed. Output of File.exe: " + output
-    }
-
-    // at this point output should look like: "text/plain; charset=us-ascii\n"
-
-    var re = /^(.+);\scharset=(.+)\n$/.exec(output);
-    if(!re) {
-      throw "Failed to parse the output"
-    }
-
-    debug("MimeType=" + re[1] + "; Encoding=" + re[2])
-
-    mimeType = re[1]
-    encoding = re[2]
+    var mimeTypeAndEncoding = fileMimeTypeDetector.DetectMimeType(filePath)
+    mimeType = mimeTypeAndEncoding.MimeType
+    encoding = mimeTypeAndEncoding.Encoding
+    debug("MimeType=" + mimeType + "; Encoding=" + encoding)
 
     mimeTypeAlias = mimeTypeAliases[mimeType]
     if(mimeTypeAlias) {
       mimeType = mimeTypeAlias
-    }
-
-    if(re[1] === "application/vnd.microsoft.portable-executable") {
-      var arch = getExeOrDllArchitecture(filePath)
-      if(arch !== null) {
-        mimeType += " " + arch
-      }
-      if(isManagedExeOrDll(filePath)) {
-        mimeType += " .Net"
-      }
-    }
-
-    if(encoding === "utf-8") {
-      var handle = fsu.OpenFile(filePath)
-      var bom = handle.Read(3)
-      handle.Close()
-      if(bom(0) === 0xEF && bom(1) === 0xBB && bom(2) === 0xBF) {
-        encoding = "utf-8 bom"
-      }
     }
 
     if(encoding === "binary" && Script.config.ignoreBinaryEncoding) {
@@ -109,86 +68,17 @@ function OnColumnDataRequested(/* ScriptColumnData */ data) {
     }
   } catch (e) {
     debug("Exception: " + e)
+    mimeType = "?"
+    encoding = "?"
   }
 
   data.columns("MIME type").value = mimeType;
   data.columns("Encoding").value = encoding;
 }
 
-function getFileMimeTypeAndEncoding(/* Path */ filePath) {
-  var command = '"' + Script.config.fileExeFullName + '" ' + "--mime --brief " + '"' + filePath + '"'
-  if(Script.config.fileExeMagicFileFullName !== "") {
-    command += ' --magic-file "' + Script.config.fileExeMagicFileFullName + '"'
-  }
-  return runCommandAndReturnOutput(command)
-}
-
-function runCommandAndReturnOutput(/* string */ command) {
-  var tempFileFullName = fsu.GetTempFilePath()
-  var cmdLine = 'cmd.exe /c "' + command + ' > "' + tempFileFullName + '""' + " 2>&1"
-  debug("shell.run " + cmdLine)
-
-  try {
-    var exitCode = shell.run(cmdLine, 0, true)
-    var output = readAllTextIfFileExists(tempFileFullName)
-    if (exitCode !== 0) {
-      throw "Failed to execute the command. ExitCode=" + exitCode + ". Output: " + output
-    }
-    return output
-  }
-  finally {
-    fso.DeleteFile(tempFileFullName)
-  }
-}
-
-function readAllTextIfFileExists(/* Path */ filePath) {
-  if(!fso.FileExists(filePath)) {
-    return ""
-  }
-
-  var handle = fsu.OpenFile(filePath)
-  var content = stt.Decode(handle.Read(), "utf8")
-  handle.Close()
-  return content
-}
-
 function isUncOrFtpPath(/* Path */ filePath) {
   var fileFullName = String(filePath)
   return fileFullName.substr(0, 2) === "\\\\" || fileFullName.substr(0, 3) === "ftp"
-}
-
-function isManagedExeOrDll(/* Path */ exeOrDllPath) {
-  var command = "powershell.exe -ExecutionPolicy Bypass -Command [System.Reflection.AssemblyName]::GetAssemblyName(\\\"" + exeOrDllPath + "\\\")"
-  debug("excecute: " + command)
-  var res = shell.run(command, 0, true);
-  return res === 0;
-}
-
-function getExeOrDllArchitecture(exeOrDllPath) {
-  var ado = new ActiveXObject("ADODB.Stream");
-  ado.Type = 1; // binary
-  ado.Open();
-  ado.LoadFromFile(exeOrDllPath);
-
-  // Read the DOS Header to find the offset to the PE header (e_lfanew)
-  ado.Position = 0x3C;
-  var eLfanewOffsetBytes = (new VBArray(ado.Read(4))).toArray();
-  var eLfanewOffset = eLfanewOffsetBytes[0] + (eLfanewOffsetBytes[1] << 8) + (eLfanewOffsetBytes[2] << 16) + (eLfanewOffsetBytes[3] << 24);
-
-  // Go to the COFF File Header (PE Signature is 4 bytes, so skip that)
-  ado.Position = eLfanewOffset + 4;
-
-  // Read the Machine field
-  var machineBytes = (new VBArray(ado.Read(2))).toArray();
-  var machine = machineBytes[0] + (machineBytes[1] << 8);
-
-  switch (machine) {
-    case 0x014c: return "x32";
-    case 0x8664: return "x64";
-    case 0x01c4: return "x32-ARM";
-    case 0xaa64: return "x64-ARM64";
-    default: return null;
-  }
 }
 
 function debug(text) {
